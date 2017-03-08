9583367e810e6b76c9d8a698810720559179f09b
ZhongYu <zhongyu54321@gmail.com>
2017-03-08 20:35:41 +0800

    update:2017-03-08 20:35:38

---
+++ src/main/java/com/zhongyu/wechat/utils/SignUtil.java
@@ -0,0 +1,50 @@
+package com.zhongyu.wechat.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+/**
+ * Created by ZhongYu on 2017/3/8.
+ */
+public class SignUtil {
+
+    private static String token = "wechat";
+
+    public static boolean checkSignature(String signature, String timestamp, String nonce) {
+        String[] arr = new String[]{token, timestamp, nonce};
+        Arrays.sort(arr);
+        StringBuilder content = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            content.append(arr[i]);
+        }
+        MessageDigest messageDigest = null;
+        String tmpStr = null;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-1");
+            byte[] digest = messageDigest.digest(content.toString().getBytes());
+            tmpStr = byteToStr(digest);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private static String byteToStr(byte[] byteArray) {
+        String strDigest = "";
+        for (int i = 0; i < byteArray.length; i++) {
+            strDigest += byteToHexStr(byteArray[i]);
+        }
+        return strDigest;
+    }
+
+    private static String byteToHexStr(byte b) {
+        char[] Digit = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        char[] tempArr = new char[2];
+        tempArr[0] = Digit[(b >>> 4) & 0X0F];
+        tempArr[1] = Digit[b & 0X0F];
+        String str = new String(tempArr);
+        return str;
+    }
+
+}
